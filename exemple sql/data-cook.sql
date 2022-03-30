5b2ecfd3a6d280eeb16be61804a7055e16c727fd
unknown <cisco9@wanadoo.fr>
2022-03-30 16:14:48 +0200

    premier commit

---
+++ exemple sql/data-cook.sql
@@ -0,0 +1,5 @@
+/* Création table Personnes*/
+CREATE TABLE Personnes(Id integer PRIMARY KEY, prenom varchar(30), age int, metier varchar(30) );
+
+/* Create few records in this table */
+INSERT INTO Personnes VALUES(1,'Tom', 28 , 'Cuisinier'), (2,'Lucy', 18 , 'Etudiante'), (3,'Frank', 31 , 'Cariste'), (4,'Jane',  21 , 'Musicienne' ), (5,'Robert', 54 , 'Chauffeur');
